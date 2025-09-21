ba64c949014e6646fa0abe8d839a30d99ae7f9be
servadmin <servadmin@dx4350>
2025-09-20 20:49:39 -0400

    Move headless false into app and add config file JVM arg

--- client/src/main/java/com/codeferm/PlayApp.java
+++ client/src/main/java/com/codeferm/PlayApp.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) Steven P. Goldsmith. All rights reserved.
+ */
 package com.codeferm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -7,34 +10,60 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import picocli.CommandLine;
 
 /**
+ * Alarmbian player app based on Spring Boot. This uses Pico CLI to handle blocking in UI code. Otherwise Spring Boot app exits
+ * right away.
  *
- * @author servadmin
+ * @author Steven P. Goldsmith
+ * @version 1.0.0
+ * @since 1.0.0
  */
 @SpringBootApplication
 @Slf4j
 public class PlayApp implements CommandLineRunner, ExitCodeGenerator {
-    
-    @Autowired
-    private PlayUI playUI;    
 
+    /**
+     * Player UI.
+     */
+    @Autowired
+    private PlayUI playUI;
+    /**
+     * App exit code.
+     */
     private int exitCode;
 
+    /**
+     * Run Pico CLI command line.
+     *
+     * @param args Arguments passed on command line.
+     * @throws Exception Possible exception.
+     */
     @Override
     public void run(String... args) throws Exception {
         exitCode = new CommandLine(playUI).execute(args);
     }
 
+    /**
+     * Get exit code.
+     *
+     * @return Exit code.
+     */
     @Override
     public int getExitCode() {
         return exitCode;
     }
 
+    /**
+     * Main program.
+     *
+     * @param args Arguments passed on command line.
+     */
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        // let Spring instantiate and inject dependencies
-        System.exit(SpringApplication.exit(SpringApplication.run(PlayApp.class, args)));
+        // Let Spring instantiate and inject dependencies
+        System.exit(SpringApplication.exit(new SpringApplicationBuilder(PlayApp.class).headless(false).run(args)));
     }
 }
